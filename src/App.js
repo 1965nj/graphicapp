a49c294fa751482602cba036584851a3cc3f317a
1965nj <jafaroun1998@gmail.com>
2024-05-07 10:56:44 +0100

    this is our initiating code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Veuillez choisir l'algorithme a executer</h1>
+      <div>
+        <button type="button" className="dijkstra">
+          DIJKSTRA
+        </button>
+      </div>
+      <div>
+        <button type="button" className="prim">
+          PRIM
+        </button>
+      </div>
+      <div>
+        <button type="button" className="kruskal">
+         KRUSKAL
+        </button>
+      </div>
     </div>
   );
 }
